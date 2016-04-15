acfd51790e481766770be49ce5917847801193da
lethanhtan89 <lethanhtan89>
2016-04-15 10:57:45 +0700

    Edit Image splash

--- app/src/main/java/mosquito/project/com/mosquiteend/SplashActivity.java
+++ app/src/main/java/mosquito/project/com/mosquiteend/SplashActivity.java
@@ -5,11 +5,7 @@ import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.transition.Fade;
-import android.transition.Slide;
 import android.widget.ImageView;
-
-import org.w3c.dom.ProcessingInstruction;
 
 /**
  * Created by Administrator on 3/31/2016.
@@ -17,7 +13,7 @@ import org.w3c.dom.ProcessingInstruction;
 public class SplashActivity extends AppCompatActivity {
     private Runnable runnable;
     private Handler handler;
-    private static final int POST_DISPLAY_TIME = 1500;
+    private static final int POST_DISPLAY_TIME = 2000;
     private ImageView imgSplash;
     private AnimationDrawable animationDrawable;
 
@@ -27,9 +23,9 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         imgSplash = (ImageView) findViewById(R.id.img_splash);
-        imgSplash.setBackgroundResource(R.drawable.splash_animation);
-        animationDrawable = (AnimationDrawable) imgSplash.getBackground();
-        animationDrawable.start();
+        //imgSplash.setBackgroundResource(R.drawable.splash_animation);
+        //animationDrawable = (AnimationDrawable) imgSplash.getBackground();
+        //animationDrawable.start();
 
         handler = new Handler();
         runnable = new Runnable() {
